350930209bb4cd5b587de895f6aebdfd91b35ace
𝐄𝐦𝐩𝐢𝐫𝐞 𝐓𝐞𝐜𝐡 [ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 ] <151479661+efeurhobobullish@users.noreply.github.com>
2025-02-07 09:59:20 +0100

    𝐸𝑀𝑃𝐼𝑅𝐸-𝑀𝐷

---
+++ pair.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const fs = require('fs');
+const { exec } = require("child_process");
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers,
+    jidNormalizedUser
+} = require("@whiskeysockets/baileys");
+const { upload } = require('./mega');
+
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+}
+
+router.get('/', async (req, res) => {
+    let num = req.query.number;
+    async function empirepair() {
+        const { state, saveCreds } = await useMultiFileAuthState(`./session`); 
+        try {
+            let empire-mdPairWeb = makeWASocket({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                },
+                printQRInTerminal: false,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: Browsers.macOS("Safari"),
+            });
+
+            if (!empire-mdPairWeb.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await empire-mdPairWeb.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            empire-mdPairWeb.ev.on('creds.update', saveCreds);
+            empire-mdPairWeb.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection === "open") {
+                    try {
+                        await delay(10000);
+                        const sessionPrabath = fs.readFileSync('./session/creds.json');
+
+                        const auth_path = './session/';
+                        const user_jid = jidNormalizedUser(empire-mdPairWeb.user.id);
+
+                      function randomMegaId(length = 6, numberLength = 4) {
+                      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                      let result = '';
+                      for (let i = 0; i < length; i++) {
+                      result += characters.charAt(Math.floor(Math.random() * characters.length));
+                        }
+                       const number = Math.floor(Math.random() * Math.pow(10, numberLength));
+                        return `${result}${number}`;
+                        }
+
+                        const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
+
+                        const string_session = mega_url.replace('https://mega.nz/file/', '');
+
+                        const sid = string_session;
+
+                        const dt = await empire-mdPairWeb.sendMessage(user_jid, { text: 'EMPIRE-MD' + sid });
+                                                // After a delay, send the additional message
+                        await delay(5000);  
+                        await empire-mdPairWeb.sendMessage(user_jid, {
+                            text: `> *PAIR CODE CONNECTED SUCCESSFULLY* ✅  
+╭───── 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 ─────────
+│⦿ *ʀᴇᴘᴏ* 
+│  https://tinyurl.com/Empire-Tech
+│────────────────────────
+│⦿ *Gʀᴏᴜᴘ* 💭  
+│ https://tinyurl.com/EMPIRE-MD-GROUP
+│────────────────────────
+│⦿ *CHANNEL* 💭  
+│ https://tinyurl.com/EMPIRE-MD-CHANNEL  
+│────────────────────────
+│⦿ *Yᴏᴜ-ᴛᴜʙᴇ* 🪄  
+│ https://youtube.com/only_one_empire  
+│────────────────────────
+│⦿ ©𝟮𝟬𝟮𝟱-𝟮𝟬𝟮𝟲 𝗘𝗺𝗽𝗶𝗿𝗲 𝗧𝗲𝗰𝗵
+╰────────────────────────`
+                        });
+
+                    } catch (e) {
+                        exec('pm2 restart empire-md');
+                    }
+
+                    await delay(100);
+                    return await removeFile('./session');
+                    process.exit(0);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
+                    await delay(10000);
+                    empirePair();
+                }
+            });
+        } catch (err) {
+            exec('pm2 restart empire-md');
+            console.log("service restarted");
+            empirepair();
+            await removeFile('./session');
+            if (!res.headersSent) {
+                await res.send({ code: "Service Unavailable" });
+            }
+        }
+    }
+    return await empirepair();
+});
+
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err);
+    exec('pm2 restart empire-md');
+});
+
+
+module.exports = router;
