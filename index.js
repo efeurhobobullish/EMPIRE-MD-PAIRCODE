417e330a154d4f649fa6f75e399f55155c75fade
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-05 19:52:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 5000;
+const qr = require('./qr'),
+const  code = require('./pair');
+require('events').EventEmitter.defaultMaxListeners = 1000;
+app.use('/qr', qr);
 app.use('/code', code);
-
-app.use('/',async (req, res, next) => {
+app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
 })
-
+app.use('/qr',async (req, res, next) => {
+res.sendFile(__path + '/qr.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/main.html')
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-    console.log(`⏩ Server running on http://localhost:` + PORT)
+    console.log(`Server running on http://localhost:` + PORT)
 })
 
-module.exports = app;
+module.exports = app
