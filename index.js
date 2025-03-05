79f0e7bca84d12a613ce60d2480c5ace2b5f3ad1
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-03-05 20:10:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let qr = require('./qr')
-let code = require('./pair');
+const qr = require('./qr')
+const code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 1000;
 app.use('/qr', qr);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
 })
-app.use('/qr',async (req, res, next) => {
+app.use('/qr-page',async (req, res, next) => {
 res.sendFile(__path + '/qr.html')
 })
 app.use('/',async (req, res, next) => {
